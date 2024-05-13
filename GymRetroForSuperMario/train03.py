c853ea8d560d136fac92d4f1fe0a60832fb9cda4
becky <satoru-kibe@rhythmcan.com>
2024-05-13 09:20:58 +0900

    - 動画撮影が正しく動くように修正 - 動画保存フォルダを日時で作成

--- GymRetroForSuperMario/train03.py
+++ GymRetroForSuperMario/train03.py
@@ -3,14 +3,17 @@ import keyboard
 from stable_baselines3 import PPO
 from gym.wrappers import Monitor
 from stable_baselines3.common.vec_env import DummyVecEnv
+from datetime import datetime
+import os
+
+
+# 定数
+game_name = 'SuperMarioBros-Nes'
+total_steps = 10000
 
 def main():
     # 環境の作成
-    env = retro.make(game='SuperMarioBros-Nes')
-
-    # Monitorでラップし、結果を保存するディレクトリを指定
-    # force=Trueを追加して、以前のモニターファイルを自動的にクリア
-    env = Monitor(env, "./results", force=True)
+    env = retro.make(game=game_name)
 
     env_train = DummyVecEnv([lambda: env])
 
@@ -20,7 +23,7 @@ def main():
     model = PPO("MlpPolicy", env_train, verbose=1)
 
     # エージェントの訓練
-    model.learn(total_timesteps=100000)
+    model.learn(total_timesteps=total_steps)
 
     print("Training finished.")
 
@@ -28,7 +31,17 @@ def main():
     env.close()
 
     # 訓練したエージェントでプレイ
-    env_play = retro.make(game='SuperMarioBros-Nes')  # プレイ用の環境
+    env_play = retro.make(game=game_name)  # プレイ用の環境
+
+    # 現在の日時を取得し、文字列に変換
+    now = datetime.now().strftime('%Y-%m-%d_%H%M%S%f')
+
+    # results配下に更にミリ秒まで指定した時間でのフォルダを作成
+    result_dir = os.path.join("./results", now)
+
+    # Monitorでラップし、結果を保存するディレクトリを指定
+    env_play = Monitor(env_play, result_dir)
+
     obs = env_play.reset()
     done = False
     while not done:
@@ -36,7 +49,7 @@ def main():
         if keyboard.is_pressed('q'):
             print("Quit key pressed. Exiting game...")
             break
-        
+
         action, _states = model.predict(obs)
         obs, reward, done, info = env_play.step(action)
         env_play.render()
@@ -48,6 +61,7 @@ def main():
     env_play.close()
 
     print("Environment closed.")
+
 
 if __name__ == "__main__":
     main()
